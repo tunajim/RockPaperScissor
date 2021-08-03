9474d15240a46c16efc137621d721db39c4aa99f
tunajim <lesperancejames@gmail.com>
2021-08-03 01:31:13 -0700

    RockPaperScissors.html

--- rps.js
+++ rps.js
@@ -1,9 +1,4 @@
 
-
-//playing against computer
-//have the computer randomly return ;rock' 'paper' or 'scissors'
-//need variable to hold random value
-
 let compMove = "";
 let moves =  ['rock', 'paper', 'scissors'];
 
@@ -11,9 +6,21 @@ console.log(moves);
 
 function computerPlay(){
     compMove = moves[Math.floor(Math.random()*moves.length)]
+
+    if(compMove==='rock'){
+        document.getElementById('computer').src="RockPaperScissors/rightRock.png";
+    }else if(compMove==='paper'){
+        document.getElementById('computer').src="RockPaperScissors/rightPaper.png";
+    }else if(compMove==='scissors'){
+        document.getElementById('computer').src="RockPaperScissors/rightScissors.png";
+    }
 }
 
-computerPlay()
+
+computerPlay();
+
+
+
 
 //write function that plays single round of rps, take two parameters, playerSelection and computerSelection
 //prompt player to choose rock paper or scissors
@@ -22,16 +29,109 @@ computerPlay()
 //test to see if player won or lost
 //store in result and return result
 let result;
+let resultMessage = document.querySelector("#resultMessage");
+console.log(resultMessage);
+resultMessage.innerHTML = "Press Button to Play!";
 
-function playRound(playerSelection, computerSelection){
+let userInput = '';
+
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+
+let win = 'You Win!';
+let lose = 'You Lose';
+let draw = 'Its a Draw!';
+
+function getInput(){
+    rock.addEventListener("click", function(){userInput = 'rock';
+        console.log(userInput);});
+    paper.addEventListener("click", function(){userInput = 'paper';
+        console.log(userInput)});
+    scissors.addEventListener("click", function(){userInput = 'scissors';
+        console.log(userInput);
+    });
+}
+
+function displayWinner(){
+    if(userInput === 'rock' && compMove === 'rock'){
+        resultMessage.innerHTML = draw;
+    }else if(userInput === 'rock' && compMove === 'paper'){
+        resultMessage.innerHTML = lose;
+    }else if(userInput === 'rock' && compMove === 'scissors'){
+        resultMessage.innerHTML = win;
+    }else if(userInput === 'paper' && compMove === 'paper'){
+        resultMessage.innerHTML = draw;
+    }else if(userInput === 'paper' && compMove === 'scissors'){
+        resultMessage.innerHTML = lose;
+    }else if(userInput === 'paper' && compMove === 'rock'){
+        resultMessage.innerHTML = win;
+    }else if(userInput === 'scissors' && compMove === 'scissors'){
+        resultMessage.innerHTML = draw;
+    }else if(userInput === 'scissors' && compMove === 'rock'){
+        resultMessage.innerHTML = lose;
+    }else if(userInput === 'scissors' && compMove === 'paper'){
+        resultMessage.innerHTML = win;
+    }
+}
+
+function updateSprites(){
+    rock.addEventListener("click", function(){
+        document.getElementById('player').src="RockPaperScissors/leftRock.png";
+        computerPlay();
+        displayWinner();
+        addScore();
+    });
+    paper.addEventListener("click", function(){
+        document.getElementById('player').src="RockPaperScissors/leftPaper.png";
+        computerPlay();
+        displayWinner();
+        addScore();
+    });
+    scissors.addEventListener("click", function(){
+        document.getElementById('player').src="RockPaperScissors/leftScissors.png";
+        computerPlay();
+        displayWinner();
+        addScore();
+    });
+}
+
+let plScore = 0;
+let compScore = 0; 
+
+function addScore() {
+    if(resultMessage.innerHTML === win){
+        plScore += 1;
+        document.getElementById('playerScore').innerHTML = plScore;
+        console.log(plScore);
+    }else if(resultMessage.innerHTML === lose){
+        compScore += 1;
+        document.getElementById('compScore').innerHTML = compScore;
+        console.log(compScore)
+    }
+}
+
+
+function addCompScore(){
+    compScore += 1;
+}
+
+function play (){
+    getInput();
+    updateSprites();
+}
+
+play();
+
+
+
+/*function playRound(playerSelection, computerSelection){
     computerSelection = compMove;
+    playerSelection = userInput;
     let winner;
-    let userInput = prompt('Please enter Rock, Paper, or Scissors');
    // userInput.toLowerCase();
-    userInput.toString();
-    playerSelection = userInput.toLowerCase(); 
+
     
-   
     
     if(playerSelection === 'rock' && compMove === 'paper'){
         result = 'You Lose! Paper beats Rock.';
@@ -52,6 +152,7 @@ function playRound(playerSelection, computerSelection){
     }
     return alert(result);
 }
+*/
 
 //create function that iterates the game 5 times
 //make computer play each time
@@ -66,7 +167,7 @@ function game(){
     alert('Thanks for playing!');
 }
 
-game()
+//game()
 
 
-playRound()
+//playRound(playerSelection, computerSelection);
